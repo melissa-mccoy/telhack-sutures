554e0976720552907d0d811466cbf16bc5a6be9b
Julia Ebert <ebertjulia@gmail.com>
2015-11-07 14:13:48 +0000

    Updates to handle photos (also change to sqlite3 db)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'games#dashboard'
   resources :games
   get "/dashboard", to: "games#dashboard", as: "dashboard"
+  get "/score_image/", to: "games#score"
 end
